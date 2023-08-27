9f595976098d1df01e1227d290ab26c8886311fe
Pio <p990301@gmail.com>
2023-08-27 20:23:11 +0900

    User API 수정 진행 1. TODO 참고 2. 요청시 json 변경 예정

--- app/api/user.py
+++ app/api/user.py
@@ -1,33 +1,71 @@
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, reqparse
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
+import os
 
-from app import api
 from app.models import user_model
 from app.databases import db, cursor
 
 # 네임스페이스 생성
 user_ns = Namespace('user', description='사용자 관련 기능', doc='/user', path='/user')
 
-# 네임스페이스에 모델 등록
-user_ns_field = user_ns.model('UserModel', user_model)
+# user_filed = user_ns.model('UserModel', user_model)
+
+# Parser for parsing the request data
+user_json_parser = reqparse.RequestParser()
+user_json_parser.add_argument('name', type=str, required=True, help='Name is required')
+user_json_parser.add_argument('email', type=str, required=True, help='Email is required')
+user_json_parser.add_argument('password', type=str, required=True, help='Password is required')
+
+user_file_parser = reqparse.RequestParser()
+user_file_parser.add_argument('profile_image', type=FileStorage, location='files')
+
+# TODO
+""" 
+1. 현재 회원가입 관련해서는 URL에 비밀번호가 노출되는 문제가 발생
+   URL에 안 보이게 해야할 것으로 생각
+   JSON 양식으로 하게 되면 Parser의 형태를 바꾸긴 해야할 것으로 생각
+"""
 
 
 @user_ns.route('/signup')
 class SignupResource(Resource):
-    @user_ns.expect(user_ns_field)
+    @user_ns.expect(user_json_parser, user_file_parser, validate=True)
     def post(self):
         """
             유저 등록
         """
-        data = api.payload
+        data = user_json_parser.parse_args()
         name = data['name']
         email = data['email']
         password = data['password']
+        profile_image = user_file_parser.parse_args()['profile_image']
+
+        # Check if user with given email already exists
+        user_query = "SELECT id FROM users WHERE email = %s"
+        cursor.execute(user_query, (email,))
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            return {'message': 'User with this email already exists'}, 400
+
+        # Save the profile image
+        if profile_image:
+            filename = secure_filename(profile_image.filename)
+            upload_dir = os.path.join("app", "profile_image", name)
+            os.makedirs(upload_dir, exist_ok=True)
+            upload_path = os.path.join(upload_dir, filename)
+            profile_image.save(upload_path)
+            profile_image_db_path = os.path.join("app", "profile_image", name, filename)
+        else:
+            profile_image_db_path = None
 
         # 사용자 정보 데이터베이스에 저장
-        query = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
-        values = (name, email, password)
+        query = "INSERT INTO users (name, email, password, profile_image) VALUES (%s, %s, %s, %s)"
+        values = (name, email, password, profile_image_db_path)
         cursor.execute(query, values)
         db.commit()
+
         return {'message': 'User registered successfully'}, 201
 
 
@@ -38,7 +76,7 @@ class UsersResource(Resource):
             모든 유저 조회
         """
         # 데이터베이스에서 모든 사용자 정보 조회
-        query = "SELECT id, name, email FROM users"
+        query = "SELECT id, name, email, profile_image FROM users"
         cursor.execute(query)
         users = cursor.fetchall()
 
@@ -47,57 +85,59 @@ class UsersResource(Resource):
             user_dict = {
                 'id': user[0],
                 'name': user[1],
-                'email': user[2]
+                'email': user[2],
+                'profile_image': user[3]
             }
             user_list.append(user_dict)
 
         return {'users': user_list}
 
 
-@user_ns.route('/user/<string:email>')
+@user_ns.route('/<string:user_email>')
 class UserResource(Resource):
-    def get(self, email):
+    def get(self, user_email):
         """
             특정 이메일을 통해 유저 조회
         """
         # 주어진 이메일로 데이터베이스에서 사용자 정보 조회
-        query = "SELECT id, name, email FROM users WHERE email = %s"
-        cursor.execute(query, (email,))
+        query = "SELECT id, name, email, profile_image FROM users WHERE email = %s"
+        cursor.execute(query, (user_email,))
         user = cursor.fetchone()
 
         if user:
             user_dict = {
                 'id': user[0],
                 'name': user[1],
-                'email': user[2]
+                'email': user[2],
+                'profile_image': user[3]
             }
             return user_dict
         else:
             return {'message': 'User not found'}, 404
 
-    @user_ns.expect(user_ns_field)
-    def put(self, email):
+    @user_ns.expect(user_json_parser, validate=True)
+    def put(self, user_email):
         """
             특정 이메일을 통해 유저 정보 수정
         """
-        data = api.payload
+        data = user_json_parser.parse_args()
         new_name = data['name']
         new_email = data['email']
         new_password = data['password']
 
         # 주어진 이메일로 기존 유저가 존재하는지 확인합니다.
         user_query = "SELECT id FROM users WHERE email = %s"
-        cursor.execute(user_query, (email,))
+        cursor.execute(user_query, (user_email,))
         existing_user = cursor.fetchone()
 
         if existing_user:
             # 사용자 정보 업데이트
             update_query = "UPDATE users SET name = %s, email = %s, password = %s WHERE email = %s"
-            update_values = (new_name, new_email, new_password, email)
+            update_values = (new_name, new_email, new_password, user_email)
 
             cursor.execute(update_query, update_values)
             db.commit()
 
             return {'message': 'User information updated successfully'}, 200
         else:
-            return {'message': 'No updates provided'}, 400
+            return {'message': 'User does not exist'}, 400

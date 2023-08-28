273d46c76a337303edfddfacd765749c8a690364
Pio <p990301@gmail.com>
2023-08-28 14:07:53 +0900

    1. Model 형태로 유형 변경 완료 2. User 프로필 이미지 추가 및 수정 가능

--- app/api/user.py
+++ app/api/user.py
@@ -1,51 +1,33 @@
 from flask_restx import Resource, Namespace, reqparse
+from flask import request, send_file
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
 import os
 
+from app import api
 from app.models import user_model
 from app.databases import db, cursor
 
 # 네임스페이스 생성
 user_ns = Namespace('user', description='사용자 관련 기능', doc='/user', path='/user')
 
-# user_filed = user_ns.model('UserModel', user_model)
-
-# Parser for parsing the request data
-user_json_parser = reqparse.RequestParser()
-user_json_parser.add_argument('name', type=str, required=True, help='Name is required')
-user_json_parser.add_argument('email', type=str, required=True, help='Email is required')
-user_json_parser.add_argument('password', type=str, required=True, help='Password is required')
+user_filed = user_ns.model('UserModel', user_model)
 
 user_file_parser = reqparse.RequestParser()
 user_file_parser.add_argument('profile_image', type=FileStorage, location='files')
 
-# TODO
-""" 
-1. 현재 회원가입 관련해서는 URL에 비밀번호가 노출되는 문제가 발생
-   URL에 안 보이게 해야할 것으로 생각
-   JSON 양식으로 하게 되면 Parser의 형태를 바꾸긴 해야할 것으로 생각
-   JWT 도입 고려
-2. 현재는 User Model 기반이 아닌 form-data URL을 통해서 Parser가 동작
-   해당 부분에 대해서 User Model을 사용하는 게 바람직하다고 생각함
-   그렇게 될 경우 File 받는 부분에 대해서 수정이 필요
-   ⇒ 로직의 변화 필요
-   File 받는 부분만 따로 분리할 것 인지 / 같이 진해할 것 인지 고려
-"""
-
 
 @user_ns.route('/signup')
 class SignupResource(Resource):
-    @user_ns.expect(user_json_parser, user_file_parser, validate=True)
+    @user_ns.expect(user_filed, validate=True)
     def post(self):
         """
             유저 등록
         """
-        data = user_json_parser.parse_args()
+        data = request.json
         name = data['name']
         email = data['email']
         password = data['password']
-        profile_image = user_file_parser.parse_args()['profile_image']
 
         # Check if user with given email already exists
         user_query = "SELECT id FROM users WHERE email = %s"
@@ -55,20 +37,9 @@ class SignupResource(Resource):
         if existing_user:
             return {'message': 'User with this email already exists'}, 400
 
-        # Save the profile image
-        if profile_image:
-            filename = secure_filename(profile_image.filename)
-            upload_dir = os.path.join("app", "profile_image", name)
-            os.makedirs(upload_dir, exist_ok=True)
-            upload_path = os.path.join(upload_dir, filename)
-            profile_image.save(upload_path)
-            profile_image_db_path = os.path.join("app", "profile_image", name, filename)
-        else:
-            profile_image_db_path = None
-
         # 사용자 정보 데이터베이스에 저장
-        query = "INSERT INTO users (name, email, password, profile_image) VALUES (%s, %s, %s, %s)"
-        values = (name, email, password, profile_image_db_path)
+        query = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
+        values = (name, email, password)
         cursor.execute(query, values)
         db.commit()
 
@@ -82,7 +53,7 @@ class UsersResource(Resource):
             모든 유저 조회
         """
         # 데이터베이스에서 모든 사용자 정보 조회
-        query = "SELECT id, name, email, profile_image FROM users"
+        query = "SELECT id, name, email FROM users"
         cursor.execute(query)
         users = cursor.fetchall()
 
@@ -92,7 +63,6 @@ class UsersResource(Resource):
                 'id': user[0],
                 'name': user[1],
                 'email': user[2],
-                'profile_image': user[3]
             }
             user_list.append(user_dict)
 
@@ -106,7 +76,7 @@ class UserResource(Resource):
             특정 이메일을 통해 유저 조회
         """
         # 주어진 이메일로 데이터베이스에서 사용자 정보 조회
-        query = "SELECT id, name, email, profile_image FROM users WHERE email = %s"
+        query = "SELECT id, name, email FROM users WHERE email = %s"
         cursor.execute(query, (user_email,))
         user = cursor.fetchone()
 
@@ -115,18 +85,17 @@ class UserResource(Resource):
                 'id': user[0],
                 'name': user[1],
                 'email': user[2],
-                'profile_image': user[3]
             }
             return user_dict
         else:
             return {'message': 'User not found'}, 404
 
-    @user_ns.expect(user_json_parser, validate=True)
+    @user_ns.expect(user_filed, validate=True)
     def put(self, user_email):
         """
             특정 이메일을 통해 유저 정보 수정
         """
-        data = user_json_parser.parse_args()
+        data = request.json
         new_name = data['name']
         new_email = data['email']
         new_password = data['password']
@@ -147,3 +116,85 @@ class UserResource(Resource):
             return {'message': 'User information updated successfully'}, 200
         else:
             return {'message': 'User does not exist'}, 400
+
+
+@user_ns.route('/<string:user_email>/profile')
+class ProfileResource(Resource):
+    def get(self, user_email):
+        """
+            특정 이메일을 통해 유저 프로필 이미지 조회
+        """
+
+        user_query = "SELECT id, name, email FROM users WHERE email = %s"
+        cursor.execute(user_query, (user_email,))
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            user = {
+                'id': existing_user[0],
+                'name': existing_user[1],
+                'email': existing_user[2],
+            }
+
+            profile_query = "SELECT id, email, src FROM profile WHERE email = %s"
+            cursor.execute(profile_query, (user['email'],))
+            profile = cursor.fetchone()
+
+            if profile:
+                return send_file(profile[2], mimetype='image/jpeg')
+
+            else:
+                return {'message': 'User profile image not found'}, 400
+
+        else:
+            return {'message': 'User not found'}, 404
+
+    @user_ns.expect(user_file_parser, validate=True)
+    def post(self, user_email):
+        """
+            특정 이메일을 통해 유저 프로필 이미지 추가
+        """
+        profile_image = user_file_parser.parse_args()['profile_image']
+
+        # Check if user with given email already exists
+        user_query = "SELECT id, name, email FROM users WHERE email = %s"
+        cursor.execute(user_query, (user_email,))
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            user = {
+                'id': existing_user[0],
+                'name': existing_user[1],
+                'email': existing_user[2],
+            }
+
+            if profile_image:
+                filename = secure_filename(profile_image.filename)
+                upload_dir = os.path.join("app", "profile_image", user['name'])
+                os.makedirs(upload_dir, exist_ok=True)
+                upload_path = os.path.join(upload_dir, filename)
+                profile_image.save(upload_path)
+                profile_image_db_path = os.path.join("profile_image", user['name'], filename)
+            else:
+                profile_image_db_path = None
+
+        else:
+            return {'message': 'User not found'}, 404
+
+        # 사용자 프로필 이미지 데이터베이스에 저장
+        query = "INSERT INTO profile (id, email, src) VALUES (%s, %s, %s)"
+        values = (user['id'], user['email'], profile_image_db_path)
+        cursor.execute(query, values)
+        db.commit()
+
+        return {'message': 'User profile image registered successfully'}, 201
+
+    def put(self, user_email):
+        """
+            특정 이메일을 통해 유저 프로필 이미지 수정
+        """
+
+    def delete(self, user_email):
+        """
+            특정 이메일을 통해 유저 프로필 이미지 삭제
+        """

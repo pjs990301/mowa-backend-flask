b9d0dafe097bca27a4a131ca3e2c636bb2d51e19
Pio <p990301@gmail.com>
2023-08-30 11:32:13 +0900

    jwt 토큰 정상 작동 확인 추후 @jwt_required 지점 논의 필요

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask_restx import Api, Resource, reqparse, Namespace
 from app.databases import db, cursor
 from .api.user import user_ns
+from .config.jwt import jwt_config_info
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = jwt_config_info
+jwt = JWTManager(app)
+
 api = Api(app, version='1.0', title='MoWA API Document', description='Check the REST API specification of MoWA.')
 
 # API에 네임스페이스 추가

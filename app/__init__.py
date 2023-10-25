59adb846f2e53f22494c419ad0f3b53ec7b4c132
Pio <p990301@gmail.com>
2023-10-26 01:01:54 +0900

    jwt 토큰 내용 롤백

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from app.config import *
 from app.databases import db, cursor
 from .api.user import user_ns
 from .config.jwt import jwt_config_info, authorizations
+from flask_jwt_extended import JWTManager
 from .api.activity import activity_ns
 
 app = Flask(__name__)
@@ -22,6 +23,7 @@ mail = Mail(app)
 
 
 app.config["JWT_SECRET_KEY"] = jwt_config_info
+jwt = JWTManager(app)
 
 api = Api(app, version='1.0', title='MoWA API Document', description='Check the REST API specification of MoWA.',
           authorizations=authorizations)
